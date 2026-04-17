de3512aa3c1369b204e706ba99afdd1ee08144b1
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-17 16:06:02 -0500

    Mover a la izq. y drch. personaje

--- limones.js
+++ limones.js
@@ -11,6 +11,7 @@ iniciar=function(){
 const ALTURA_SUELO=50;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
 
 dibujarSuelo=function(){
     ctx.fillStyle="green"
@@ -19,5 +20,27 @@ dibujarSuelo=function(){
 
 dibujarPersonaje=function(){
     ctx.fillStyle="#00DBFC"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+let velocidadPersonaje=20;
+
+moverIzquierda=function(){
+    personajeX=personajeX-velocidadPersonaje;
+    actualizarPantalla();
+}
+
+moverDerecha=function(){
+    personajeX=personajeX+velocidadPersonaje;
+    actualizarPantalla();
+}
+
+actualizarPantalla=function(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+limpiarCanva=function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
